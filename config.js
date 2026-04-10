f218214a05cd573e2b28a05d60a3fdd262fb1b77
deepkoley156 <deepkoley156@gmail.com>
2026-04-11 00:02:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  BOT_USERNAME: "ExtraPe Link Converter Bot", // এটা display name না, username লাগবে
+  BOT_USERNAME_FALLBACK: "ExtraPeLinkConverterBot", // username না জানলে পরে ঠিক করবে
+  REPLY_TIMEOUT_MS: 15000,
+  POLL_INTERVAL_MS: 1000,
+  ALLOWED_DOMAINS: [
+    "flipkart.com",
+    "amazon.in",
+    "amazon.com",
+    "amzn.in",
+    "amzn.to"
+  ]
+};
