f5afab89413cda431d0f90b72fb0d59a2116b46d
deepkoley156 <deepkoley156@gmail.com>
2026-04-11 16:31:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 module.exports = {
   BOT_USERNAME: "@ExtraPeBot",
   BOT_USERNAME_FALLBACK: "ExtraPeBot",
-  REPLY_TIMEOUT_MS: 15000,
-  POLL_INTERVAL_MS: 1000,
+
   ALLOWED_DOMAINS: [
     "flipkart.com",
     "www.flipkart.com",
     "dl.flipkart.com",
     "amazon.in",
     "www.amazon.in",
-    "amazon.com",
-    "www.amazon.com",
-    "amzn.in",
-    "amzn.to"
-  ]
+    "amzn.in"
+  ],
+
+  REPLY_TIMEOUT_MS: 15000,
+  POLL_INTERVAL_MS: 1000
 };
